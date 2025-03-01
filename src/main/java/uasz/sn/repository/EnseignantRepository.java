e5d145fdc37c038852f51314a3e83eb590749e5d
AllaGueye0608 <alla0608gueye@gmail.com>
2025-03-01 23:03:52 +0000

    Update EnseignantRepository.java

--- src/main/java/uasz/sn/repository/EnseignantRepository.java
+++ src/main/java/uasz/sn/repository/EnseignantRepository.java
@@ -1,5 +1,7 @@
 package uasz.sn.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import uasz.sn.model.Enseignant;
 
 @RepositoryRestResource
